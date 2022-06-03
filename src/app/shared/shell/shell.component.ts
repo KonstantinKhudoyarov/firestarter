22719d85cc34531a00b24975dfdf265f04709026
KonstantinKhudoyarov <kostya040390@gmail.com>
2022-06-03 19:40:57 +0300

    Add shell component

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import {map, shareReplay} from "rxjs";
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss']
 })
-export class ShellComponent implements OnInit {
+export class ShellComponent {
 
-  constructor() { }
+  constructor(private readonly breakpointObserver: BreakpointObserver) { }
 
-  ngOnInit(): void {
-  }
-
+  isHandset$ = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 }
