0fb27a3c6acb752a417cd828258a774232b9d286
KonstantinKhudoyarov <kostya040390@gmail.com>
2022-06-06 20:24:43 +0300

    Init login component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginPageComponent
+  ],
   imports: [
     CommonModule,
     UserRoutingModule
