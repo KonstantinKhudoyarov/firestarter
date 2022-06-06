0db4934f9026a8f9ce0268d127330bf9995bea44
KonstantinKhudoyarov <kostya040390@gmail.com>
2022-06-06 19:54:50 +0300

    Add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from "./shared/shared.module";
 import { HomeComponent } from './home/home.component';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -17,7 +20,10 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'firestarter-fcca9',
+    appId: '1:1029068241084:web:54ec4c17bf94fab0e0f8b9',
+    databaseURL: 'https://firestarter-fcca9-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'firestarter-fcca9.appspot.com',
+    apiKey: 'AIzaSyCnDfQOYY8AsVuDR1_NwjmJK5ZV6C-n2fw',
+    authDomain: 'firestarter-fcca9.firebaseapp.com',
+    messagingSenderId: '1029068241084',
+  },
+  production: true,
 };
