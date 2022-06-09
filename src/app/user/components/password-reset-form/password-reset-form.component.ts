e2fd7bc6600ab3f601df4a477ed2a0bfd70ce200
KonstantinKhudoyarov <kostya040390@gmail.com>
2022-06-09 20:15:37 +0300

    Add login form

---
+++ src/app/user/components/password-reset-form/password-reset-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-reset-form',
+  templateUrl: './password-reset-form.component.html',
+  styleUrls: ['./password-reset-form.component.scss']
+})
+export class PasswordResetFormComponent {
+
+  constructor() { }
+
+}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
-import { LoginPageComponent } from './components/login-page/login-page.component';
 import { SharedModule } from "../shared/shared.module";
+import { ReactiveFormsModule } from "@angular/forms";
 import { Authentication} from "./models/auth.model";
 import { GoogleAuthService } from "./services";
+import { LoginPageComponent } from './components/login-page/login-page.component';
 import { GoogleAuthComponent } from './components/google-auth/google-auth.component';
-import { EmailLoginComponent } from './components/email-login/email-login.component';
-
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { SignUpFormComponent } from "./components/sign-up-form/sign-up-form.component";
+import { PasswordResetFormComponent } from "./components/password-reset-form/password-reset-form.component";
 
 @NgModule({
   declarations: [
     LoginPageComponent,
     GoogleAuthComponent,
-    EmailLoginComponent
+    LoginFormComponent,
+    SignUpFormComponent,
+    PasswordResetFormComponent
   ],
   imports: [
     CommonModule,
     UserRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ],
   providers: [
     {
